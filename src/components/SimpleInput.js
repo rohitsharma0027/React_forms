b08309780233a46b790fea8a3752998dac31893f
rohitsharma0027 <developer.rohitsharmaj7@gmail.com>
2021-06-09 06:45:14 +0530

    collecting input value using useState

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,23 @@
+import {useState, useRef} from 'react';
+
 const SimpleInput = (props) => {
+
+  const [enteredName,setEnteredName] = useState()
+
+  const inputNameChangeHandler = (event)=>{
+     setEnteredName(event.target.value)
+  }
+
+  const formSubmittionHandler = (event) =>{
+    event.preventDefault()
+    console.log(enteredName)
+  }
+  
   return (
-    <form>
+    <form onSubmit={formSubmittionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input type='text' id='name' onChange={inputNameChangeHandler}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
