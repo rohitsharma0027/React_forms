7fe2f4b8317b78ca671a5ff16aadd3b3e12eae88
rohitsharma0027 <developer.rohitsharmaj7@gmail.com>
2021-06-09 06:48:02 +0530

    using ref to fetch the input value

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,6 +3,7 @@ import {useState, useRef} from 'react';
 const SimpleInput = (props) => {
 
   const [enteredName,setEnteredName] = useState()
+  const nameInputRef = useRef();
 
   const inputNameChangeHandler = (event)=>{
      setEnteredName(event.target.value)
@@ -11,13 +12,14 @@ const SimpleInput = (props) => {
   const formSubmittionHandler = (event) =>{
     event.preventDefault()
     console.log(enteredName)
+    console.log(nameInputRef.current.value)
   }
-  
+
   return (
     <form onSubmit={formSubmittionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={inputNameChangeHandler}/>
+        <input ref={nameInputRef} type='text' id='name' onChange={inputNameChangeHandler}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
